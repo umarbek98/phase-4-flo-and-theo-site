6edc1104316020d06eaff9a593c7304e32495948
Sani Rogers <sanirogers138@gmail.com>
2023-03-27 17:23:46 -0400

    do mask

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -7,7 +7,12 @@ const Hero = () => {
             <div className={styles.heroImageContainer}>
                 <img src={require("../images/FirstHero.jpeg")} />
                 <img src={require("../images/SecondHero.jpg")} />
+                <div className={styles.heroMask}>
+
+                </div>
+                
             </div>
+            
             <CallToAction />
         </div>
     )
