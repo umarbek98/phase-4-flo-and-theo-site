051f7a8add83dd7df342e3548ec88abf5b1c7cc1
Sani Rogers <sanirogers138@gmail.com>
2023-03-29 09:37:31 -0400

    do orders

--- client/src/components/FooterLinks.js
+++ client/src/components/FooterLinks.js
@@ -1,13 +1,16 @@
 import styles from "./FooterLinks.module.css";
+import { useNavigate } from "react-router-dom";
 
 const FooterLinks = () => {
 
+    const navigate = useNavigate()
+
     return (
         <div className={styles.footerLinkContainer}>
-            <a href="#">Blog</a>
-            <a href="#">Orders</a>
-            <a href="#">FAQ</a>
-            <a href="#">Privacy Policy</a>
+            <a onClick={() => navigate("/blog")}>Blog</a>
+            <a onClick={() => navigate("/orders")}>Orders</a>
+            <a onClick={() => navigate("/faq")}>FAQ</a>
+            <a onClick={() => navigate("/privacypolicy")}>Privacy Policy</a>
         </div>
     );
 }
