3091c212d025d1810626b3d920f5198a4d9166f1
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-03-30 16:47:03 -0400

    fix header image

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,11 +1,10 @@
 import CallToAction from "./CallToAction";
 import styles from "./Hero.module.css";
 const Hero = () => {
-    
+
     return(
         <div>
             <div className={styles.heroImageContainer}>
-                <img src={require("../images/FirstHero.jpeg")} />
                 <img src={require("../images/SecondHero.jpg")} />
                 <div className={styles.heroMask}>
 
