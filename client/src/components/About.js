34862c9ccd057a5e8c2ebbd65a0ac9c0ff95d971
Elisabeth Aka <aea536@nyu.edu>
2023-03-30 11:30:26 -0400

    final

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -17,12 +17,12 @@ const About = () => {
             Born in Nigeria and raised in England and Ireland, the two sisters now call New York home.
             FLO+THEO is named after two great heroes in their lives, the FLO stands for florence, their mother's name whilst the THEO is the honor and memory of their Uncle. </p>
         
+            <h3>MEET THE TEAM</h3>
             </div>
             
-            <h3>MEET THE TEAM</h3>
 
             <div className ="photo" >
-                <img src = {require("../images/Mayowa-Cofounder.jpg")} style={{height:500}}/>
+                <img src = {require("../images/Mayowa-Cofounder.jpg")} style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', height: '500px', }}/>
             </div>
 
             <div className = {styles.mayowaContainer}>
@@ -34,13 +34,13 @@ const About = () => {
                 new vegan restaurants or taking a fitness class.</p>
             </div>
             <div >
-                <img class = "picture" src = {require("../images/Ebony Cofounder.jpg")} height ={700} width={700} style={{align: 'auto'}} />
+                <img class = "picture" src = {require("../images/Ebony Cofounder.jpg")} height ={620} width={770} style={{display: 'block', marginLeft: 'auto', marginRight: 'auto'}} />
             </div>
             <div className = {styles.ebonyContainer}>
                 <h3>EBONY, CO-FOUNDER</h3>
                 <p> Inspired and passionate about all things beauty, co-founder Ebony left a career in law, in order to follow her dreams, 
                 of starting FLO+THEO. Now, instead of finding her in a courtroom, you can now find her and her sister, around markets in NYC, introducing vegan, artisanal skin care 
-                products to savvvy New Yorkers. Ebony enjoys entrepreneurship( thinks being a co-founder of FLO+THEO is awesome), photography
+                products to savvvy New Yorkers. Ebony enjoys entrepreneurship (thinks being a co-founder of FLO+THEO is awesome), photography
                 and looking ofr delicious and strange things to eat in NYC.  
 
                 </p>
